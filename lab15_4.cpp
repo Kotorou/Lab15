92edb506c890a646205790e0b6034d2a7c736c17
Kotaro <kota.kawaka@gmail.com_>
2025-01-28 15:37:09 +0700

    lab15.4 finish

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,17 @@ int main(){
 }
 
 //Do not modify source code above this line
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int x[] = {a,b,c,d};
+	for (int i = 0; i<4;i++){
+		int random = rand()%4;
+		int temp = x[i];
+		x[i] = x[random];
+		x[random] = temp;
+	}
+	a = x[0];
+	b = x[1];
+	c = x[2];
+	d = x[3];
+}
